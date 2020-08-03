672a9d89da091ab906639e62d442e4a035b84aff
Siil <jiri.jezek78@gmail.com>
2020-08-03 20:00:26 +0200

    initial commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+          <h1>hello from TodoItem</h1>
+        )
+    }
+}
